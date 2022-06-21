90d2f98c3346159220c3963f3a07db4ea9d0f4ca
asg1997 <34771066+asg1997@users.noreply.github.com>
2022-06-21 22:38:15 +0300

    finished ver 1

---
+++ lib/presentation/webview_screen/webview_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebviewScreen extends StatelessWidget {
+  const WebviewScreen({Key? key, required this.url}) : super(key: key);
+
+  final String url;
+  // WebViewController? _controller;
+  // double _height = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        // onWebViewCreated: (controller) {
+        //   _controller = controller;
+        //   controller.loadUrl(url);
+        // },
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
